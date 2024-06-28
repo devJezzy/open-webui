657047ffb64ca4842698cbcedd7c3087d7212223
devJezzy <developerjezzy@gmail.com>
2024-06-28 10:56:50 +0530

    changed default configuration

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,8 +5,8 @@ export const APP_NAME = 'Open WebUI';
 
 // const VM_HOSTNAME = '34.16.194.133'
 export const WEBUI_HOSTNAME = browser ? (dev ? `${location.hostname}:8080` : ``) : '';
-// export const WEBUI_BASE_URL = browser ? (dev ? `http://${WEBUI_HOSTNAME}` : ``) : ``;
-export const WEBUI_BASE_URL = 'https://34.16.194.133/'
+export const WEBUI_BASE_URL = browser ? (dev ? `http://${WEBUI_HOSTNAME}` : ``) : ``;
+// export const WEBUI_BASE_URL = 'https://34.16.194.133/'
 export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v1`;
 
 export const OLLAMA_API_BASE_URL = `${WEBUI_BASE_URL}/ollama`;
